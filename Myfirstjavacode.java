dc7009a9aa1c494aadce07b3cd1f1a8e7941f115
Sankalpa Das <d2sankalpa@gmail.com>
2022-08-09 16:36:19 +0530

    Add files via upload

---
+++ Myfirstjavacode.java
@@ -0,0 +1,9 @@
+// class class name--- class -- c always in small
+class Myfirstjavacode{
+	
+	public static void main (String []args){
+		
+		System.out.println("Welcome to CODING WORLD.");
+}
+
+}
