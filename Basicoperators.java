7df5c703694dc63a02c4470b4ce7be3bdca208ed
Sankalpa Das <d2sankalpa@gmail.com>
2022-08-09 17:49:43 +0530

    Add files via upload

---
+++ Basicoperators.java
@@ -0,0 +1,16 @@
+class Basicoperators {
+public static void main(String ...args) {
+System.out.println("The two number is : ");
+int a = 12; // initialize 1st local variable
+int b = 6; // initialize 2nd local variable
+int sum = a+b; // logic of addition
+int sub = a-b; // logic of subtraction
+int mul = a*b; // logic of multiplication
+int div = (a/b); // logic of division
+System.out.println(a + " " + b); // 12,6
+System.out.println("The sum is : " + sum); // printing the result of addition
+System.out.println("The subtraction is : " + sub); // printing the result of subtraction
+System.out.println("The multiplication is : " + mul); // printing the result of multiplication
+System.out.println("The multiplication is : " + div); // printing the result of division
+}
+}
