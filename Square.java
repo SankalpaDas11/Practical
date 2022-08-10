e5f8d2f9c0bbf19ac6040f5159c45caec57d9324
Sankalpa Das <d2sankalpa@gmail.com>
2022-08-10 15:21:10 +0530

    Add files via upload

---
+++ Square.java
@@ -0,0 +1,11 @@
+class Square {
+public static void main(String ...args) {
+System.out.println("The length of the sides are : ");
+int side = 10; // initialize 1st local variable
+int perimeter = (side * 4); // logic to find perimeter
+int area = (side * side); // logic to find area
+System.out.println(side); // printing the length of sides
+System.out.println("The perimeter is : " + perimeter); // printing the result of perimeter
+System.out.println("The area is : " + area); // printing the result of area
+}
+}
