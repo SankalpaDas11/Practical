c6c3064b5625cd4a375350fab660f1277e3a9484
Sankalpa Das <d2sankalpa@gmail.com>
2022-08-17 14:54:42 +0530

    Add files via upload

---
+++ Arithmeticop.java
@@ -0,0 +1,15 @@
+package Operator;
+
+public class Arithmeticop {
+	public static void main (String[] arg) {
+		int a = 10; // initializing 1st variable
+		int b = 20; // initializing 2nd variable
+		System.out.println(a+b); // addition
+		System.out.println(a-b); // subtraction
+		System.out.println(a*b); // multiplication
+		System.out.println(a/b); // division
+		System.out.println(a%b); //mod
+		System.out.println(a<b&&b<a); // true && false = false
+		System.out.println(a<b||b<a); // true || false = true
+	}
+}
