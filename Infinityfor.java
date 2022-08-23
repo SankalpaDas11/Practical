88fddcccc2ca8b427b132f80b4dabedf88ba2923
Sankalpa Das <d2sankalpa@gmail.com>
2022-08-23 17:14:48 +0530

    Add files via upload

---
+++ Infinityfor.java
@@ -0,0 +1,10 @@
+public class Infinityfor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(;;) {
+			System.out.println("Need more practice");
+		}
+	}
+
+}
