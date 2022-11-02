1be33303d03e37057c9c648c24d9372eba758f76
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-02 21:24:01 +0530

    Update RunnableState.java

--- 28th Oct - Thread/RunnableState.java
+++ 28th Oct - Thread/RunnableState.java
@@ -5,9 +5,9 @@ public class RunnableState implements Runnable{
 	public void run() {
 		System.out.println("Thread Activated");
 	}
-	public static void main(String[] args) { // main method
+	public static void main(String[] args) { 		// main method
 		RunnableState obj = new RunnableState();
-		Thread t1 = new Thread(obj); //object of thread
-		t1.start(); //starting thread
+		Thread t1 = new Thread(obj); 			//object of thread
+		t1.start(); 					//starting thread
 	}
 }
