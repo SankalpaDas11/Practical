ec0dbcfe68fafefdf88e87bc96f8904902a4015a
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-02 21:25:24 +0530

    Update ThreadCons.java

--- 28th Oct - Thread/ThreadCons.java
+++ 28th Oct - Thread/ThreadCons.java
@@ -2,9 +2,9 @@ package Thread;
 
 public class ThreadCons {
 	public static void main(String[] args) {
-		Thread obj = new Thread("Sankalpa"); // using the constructor Thread (String name)
-		obj.start(); // move to active state
-		String str = obj.getName(); //thread name by invoking getName method
+		Thread obj = new Thread("Sankalpa"); 	// using the constructor Thread (String name)
+		obj.start(); 				// move to active state
+		String str = obj.getName(); 		//thread name by invoking getName method
 		System.out.println(str); 
 	}
 }
