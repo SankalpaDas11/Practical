18b6a72a0c3c5804f995bad5f517ea284bb5e1e7
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-14 12:25:21 +0530

    Update ArrayListCompareEg.java

--- 11th Nov - ArrayList/ArrayListCompareEg.java
+++ 11th Nov - ArrayList/ArrayListCompareEg.java
@@ -1,12 +1,12 @@
 package ArrayList;
 
-import java.util.ArrayList; // importing arraylist
+import java.util.ArrayList; 						// importing arraylist
 
 public class ArrayListCompareEg {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<String>al1 = new ArrayList<String>(); // taking one string type arraylist named al1
+		ArrayList<String>al1 = new ArrayList<String>(); 	// taking one string type arraylist named al1
 		
 		//adding elements in the arraylist
 		al1.add("Rose");
@@ -17,7 +17,7 @@ public class ArrayListCompareEg {
 		al1.add("Marigold");
 		
 		
-		ArrayList<String>al2 = new ArrayList<String>(); // taking another string type arraylist named al2
+		ArrayList<String>al2 = new ArrayList<String>(); 	// taking another string type arraylist named al2
 		
 		//adding elements in the arraylist
 		al2.add("Rose");
@@ -27,12 +27,12 @@ public class ArrayListCompareEg {
 		al2.add(2,"Daisy");
 		al2.add("Marigold");
 		
-		boolean b1 = al1.equals(al2); // taking a boolean type variable to check whether both the arraylists are equal
-		System.out.println(b1); // printing that boolean variable to show result
+		boolean b1 = al1.equals(al2); 				// taking a boolean type variable to check whether both the arraylists are equal
+		System.out.println(b1); 				// printing that boolean variable to show result
 		
-		al1.add("lotus"); // adding an extra element into 1st arraylist
-		boolean b2 = al1.equals(al2); // checking for the 2nd time
-		System.out.println(b2); // showing result
+		al1.add("lotus"); 					// adding an extra element into 1st arraylist
+		boolean b2 = al1.equals(al2); 				// checking for the 2nd time
+		System.out.println(b2);					// showing result
 	}
 
 }
