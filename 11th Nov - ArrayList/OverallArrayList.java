ab64320049e6a2cd9f374c1640e8625e81e6ff9a
Sankalpa Das <d2sankalpa@gmail.com>
2022-11-15 12:36:42 +0530

    Update OverallArrayList.java

--- 11th Nov - ArrayList/OverallArrayList.java
+++ 11th Nov - ArrayList/OverallArrayList.java
@@ -1,18 +1,18 @@
 package ArrayList;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayList; 	// importing ArrayList
+import java.util.List;		// importing list
 
 public class OverallArrayList {
 	public static void main(String[] args) {
-		List al = new ArrayList(); //non generic
-		List<String> al1 = new ArrayList<String>(); // dclaring arraylist using list
+		List al = new ArrayList(); 			//non generic
+		List<String> al1 = new ArrayList<String>(); 	// dclaring arraylist using list
 		
 		//method add()
 		al.add("Rose");
 		al.add("Sunflower");
 		al.add("Rose");
-		al.add(0,"Lily"); // adding elements to a specified index location
+		al.add(0,"Lily"); 	// adding elements to a specified index location
 		al.add(2,"Daisy");
 		al.add("Marigold");
 		al.add(6,"Tulip");
